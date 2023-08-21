5ab6550e53fa01b4e331974f358485bfb2cffba6
Xander1540 <adityaxxx2003@gmail.com>
2023-08-21 15:04:43 +0530

    Add existing file

--- QRcode.py
+++ QRcode.py
@@ -1,3 +1,11 @@
 import qrcode as qr
-img=qr.make("https://youtu.be/NaQ_4ZvCbOE")
-img.save("j.png")
+from PIL import Image
+q=qr.QRCode(version=1, 
+            error_correction=qr.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,)
+
+q.add_data("https://youtu.be/NaQ_4ZvCbOE")
+q.make(fit=True)
+img= q.make_image(fill_color='darkblue', back_color='steelblue')
+img.save("x.png")
